18687825b8e9284829d6b9a3894eba5ac31447c2
yamasaki <yamasakipann0218@gmail.com>
2023-04-22 17:03:50 +0900

    fix: del log

--- server/log/log.go
+++ server/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"log"
 	"os"
 
 	"github.com/seipan/logdis"
@@ -9,8 +8,5 @@ import (
 
 func New() *logdis.Logger {
 	logger := logdis.NewLogger(os.Getenv("WEBHOOKURL"), os.Getenv("LOGIMG"), os.Getenv("LOGNAME"))
-	log.Println(os.Getenv("WEBHOOKURL"))
-	log.Println(os.Getenv("LOGIMG"))
-	log.Println(os.Getenv("LOGNAME"))
 	return logger
 }

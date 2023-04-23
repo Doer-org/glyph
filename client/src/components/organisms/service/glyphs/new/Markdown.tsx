409d2961af4cac3e29298bb709e4598b22da78ad
miso-devel <kengo279@icloud.com>
2023-04-23 18:32:18 +0900

    シンタックスハイライトの設定

--- client/src/components/organisms/service/glyphs/new/Markdown.tsx
+++ client/src/components/organisms/service/glyphs/new/Markdown.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import "./Markdown.css";
+import { CodeBlock } from "./CodeBlock";
 export const Markdown: FC = () => {
 	const [markdown, setMarkdown] = useState<string>("");
 	console.log("markdown!!");
@@ -15,7 +15,11 @@ export const Markdown: FC = () => {
 			/>
 
 			<div className="p-2 border-2  w-full">
-				<ReactMarkdown unwrapDisallowed={false} className="prose lg:prose-md">
+				<ReactMarkdown
+					unwrapDisallowed={false}
+					className="prose lg:prose-md"
+					components={CodeBlock}
+				>
 					{markdown}
 				</ReactMarkdown>
 			</div>

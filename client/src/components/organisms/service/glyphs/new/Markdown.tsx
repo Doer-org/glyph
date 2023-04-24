5c0aa94c8fa8a78a88aaba9c39e2c6315accff3d
miso-devel <kengo279@icloud.com>
2023-04-25 03:43:33 +0900

    imageをアップロードできるようにした

--- client/src/components/organisms/service/glyphs/new/Markdown.tsx
+++ client/src/components/organisms/service/glyphs/new/Markdown.tsx
@@ -1,41 +1,30 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useMemo, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { CodeBlock } from "./CodeBlock";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import {
+	handleImageDrop,
+	handleMarkdownChange,
+	options,
+} from "./MarkdownUtils";
+
 export const Markdown: FC = () => {
 	const [markdown, setMarkdown] = useState<string>("");
-	console.log("markdown!!");
+
 	return (
 		<div>
-			<textarea
-				onChange={(e) => setMarkdown(e.target.value)}
-				className="border-2 p-2 w-full h-80"
+			<SimpleMDE
+				id="simple-mde"
 				value={markdown}
-				onDrop={(e) => {
-					console.log();
-				}}
-				onDragOver={(e) => {
-					e.preventDefault();
-					const files = e.dataTransfer.files;
-					console.log(files);
-					if (files.length === 0) return;
-					const file = files[0];
-					if (file.type.startsWith("image/")) {
-						console.log(file);
-					} else {
-						alert("画像ファイルをドロップしてください。");
-					}
-				}}
-				draggable={true}
+				onChange={(value) => handleMarkdownChange(value, setMarkdown)}
+				options={options}
+				events={{ drop: handleImageDrop }}
 			/>
-
 			<div className="p-2 border-2  w-full">
-				<ReactMarkdown
-					unwrapDisallowed={false}
-					className="prose lg:prose-md"
-					components={CodeBlock}
-				>
+				<ReactMarkdown className="prose lg:prose-md" components={CodeBlock}>
 					{markdown}
 				</ReactMarkdown>
 			</div>

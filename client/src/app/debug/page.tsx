824ff564562e821bbfe576c63eae5bc5821ebb9c
miso-devel <kengo279@icloud.com>
2023-04-24 21:41:26 +0900

    debugページに一応残しておく

--- client/src/app/debug/page.tsx
+++ client/src/app/debug/page.tsx
@@ -7,10 +7,10 @@ import React from "react";
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
 export default function Debug() {
-	const [value, setValue] = useState("**Hello world!!!**");
+	const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
 	return (
 		<div>
-			<MDEditor value={value} onChange={() => setValue} />
+			<MDEditor value={value} onChange={setValue} />
 		</div>
 	);
 }

0300d1873a106f34ffe81f6b67d9f5854ca2b7d5
miso-devel <kengo279@icloud.com>
2023-04-26 00:32:01 +0900

    作成ページ改良

--- client/src/app/service/glyphs/new/page.tsx
+++ client/src/app/service/glyphs/new/page.tsx
@@ -1,8 +1,16 @@
+import { Txt } from "@/components/atoms/Txt";
 import dynamic from "next/dynamic";
 const Markdown = dynamic(
 	() => import("@/components/organisms/service/glyphs/new/Markdown"),
 	{ ssr: false },
 );
 export default function New() {
-	return <Markdown />;
+	return (
+		<>
+			<Txt elm="h2" size="text-3xl" className="text-center pb-10">
+				Glyph作成
+			</Txt>
+			<Markdown />
+		</>
+	);
 }

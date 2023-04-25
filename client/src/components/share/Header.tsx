34485226fd31c1f651203f14b890ec96205ca278
miso-devel <kengo279@icloud.com>
2023-04-26 00:21:52 +0900

    sidebarの調整

--- client/src/components/share/Header.tsx
+++ client/src/components/share/Header.tsx
@@ -4,7 +4,7 @@ import { Txt } from "../atoms/Txt";
 
 export const Header = () => {
 	return (
-		<header className="bg-slate-900 w-screen px-9 py-3 shadow-2xl text-white fixed z-10">
+		<header className="bg-slate-900 w-screen px-9 py-3 shadow-2xl text-white fixed z-50">
 			<div className="flex items-center gap-3">
 				<SvgLogo />
 				<Txt elm="h1" size="text-2xl">

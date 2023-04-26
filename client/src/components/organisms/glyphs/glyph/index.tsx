76924de6aba141fb11de4980b1df0b0ca605e228
miso-devel <kengo279@icloud.com>
2023-04-26 16:47:35 +0900

    react-icon導入

--- client/src/components/organisms/glyphs/glyph/index.tsx
+++ client/src/components/organisms/glyphs/glyph/index.tsx
@@ -1,28 +1,23 @@
 import { Txt } from "@/components/atoms/Txt";
 import { TGlyph } from "@/types/Glyph";
 import { FC } from "react";
-
+import { format } from "date-fns";
+import { FcDocument } from "react-icons/fc";
 type TProps = {
 	glyph: TGlyph;
 };
 
 export const Glyph: FC<TProps> = ({ glyph }) => {
-	const changeData = (date: Date) => {
-		const year = date.getFullYear();
-		const month = ("0" + (date.getMonth() + 1)).slice(-2);
-		const day = ("0" + date.getDate()).slice(-2);
-		return `${year}-${month}-${day}`;
-	};
 	return (
 		<div className="border border-black rounded-md m-2 w-2/3 flex justify-center">
-			<div className="border-r p-10 border-black">doc</div>
+			<div className="border-r">
+				<FcDocument size={200} />
+			</div>
 			<div className="p-5">
 				<Txt elm="h2" size="text-2xl">
 					{glyph.title}
 				</Txt>
-				<p>{changeData(glyph.updated_at)}</p>
-				<p>{glyph.content}</p>
-				<p>{glyph.author_id}</p>
+				<p>{format(glyph.updated_at, "yyyy/MM/dd")}</p>
 			</div>
 		</div>
 	);

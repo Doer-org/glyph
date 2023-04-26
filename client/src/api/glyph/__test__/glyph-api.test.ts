421cfd93dce07913b0ada8189ab2da75b768a92a
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-27 00:12:23 +0900

    test(api-client): glyph-api

--- client/src/api/glyph/__test__/glyph-api.test.ts
+++ client/src/api/glyph/__test__/glyph-api.test.ts
@@ -1,4 +1,4 @@
-import { createGlyph, editGlyph, readGlyph } from '../../../api';
+import { createGlyph, deleteGlyph, editGlyph, readGlyph } from '../';
 import { expect, test } from 'vitest';
 
 test('Glyph：Create', async () => {
@@ -10,46 +10,56 @@ test('Glyph：Create', async () => {
     prev_glyph: 'string',
     next_glyph: 'string',
   });
-  console.log(resp.type === 'ok' && resp.value.data);
   expect(resp.type).toBe('ok');
 });
 
-// test('Glyph：Create => Read', async () => {
-//   const resp = await createGlyph({
-//     author_id: 'string',
-//     title: 'string',
-//     content: 'string',
-//     status: 'Draft',
-//     prev_glyph: 'string',
-//     next_glyph: 'string',
-//   });
-//   if (resp.type === 'error') {
-//     throw new Error('Create Glyph failed');
-//   }
-//   console.log(resp.value.id)
-//   const resp2 = await readGlyph({ id: resp.value.id });
-//   expect(resp2.type).toBe('ok');
-// });
+test('Glyph：Create => Read', async () => {
+  const resp = await createGlyph({
+    author_id: 'string',
+    title: 'string',
+    content: 'string',
+    status: 'Draft',
+    prev_glyph: 'string',
+    next_glyph: 'string',
+  });
+  if (resp.type === 'error') throw new Error('Create Glyph failed');
+  const resp2 = await readGlyph(resp.value.data.id);
+  expect(resp2.type).toBe('ok');
+});
 
-// test('Glyph：Create => Edit', async () => {
-//   const resp = await createGlyph({
-//     author_id: 'string',
-//     title: 'string',
-//     content: 'string',
-//     status: 'Draft',
-//     prev_glyph: 'string',
-//     next_glyph: 'string',
-//   });
-//   if (resp.type === 'error') {
-//     throw new Error('Create Glyph failed');
-//   }
-//   const resp2 = await editGlyph({
-//     id: resp.value.id,
-//     title: 'new title',
-//     content: 'content',
-//     status: 'Draft',
-//     prev_glyph: 'string',
-//     next_glyph: 'string',
-//   });
-//   expect(resp2.type === 'ok' && resp2.value.content === 'new title').toBe(true);
-// });
+test('Glyph：Create => Edit', async () => {
+  const resp = await createGlyph({
+    author_id: 'string',
+    title: 'string',
+    content: 'string',
+    status: 'Draft',
+    prev_glyph: 'string',
+    next_glyph: 'string',
+  });
+  if (resp.type === 'error') throw new Error('Create Glyph failed');
+  const resp2 = await editGlyph({
+    id: resp.value.data.id,
+    title: 'new title',
+    content: 'content',
+    status: 'Draft',
+    prev_glyph: 'string',
+    next_glyph: 'string',
+  });
+  expect(resp2.type === 'ok').toBe(true);
+});
+
+test('Glyph：Create => Read => Delete', async () => {
+  const resp = await createGlyph({
+    author_id: 'string',
+    title: 'string',
+    content: 'string',
+    status: 'Draft',
+    prev_glyph: 'string',
+    next_glyph: 'string',
+  });
+  if (resp.type === 'error') throw new Error('Create Glyph failed');
+  const resp2 = await readGlyph(resp.value.data.id);
+  if (resp2.type === 'error') throw new Error('Read Glyph failed');
+  const resp3 = await deleteGlyph(resp2.value.data.id);
+  expect(resp3.ok).toBe(true);
+});

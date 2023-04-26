91f667d57ce7ccfa4fa7e3c19f7de4aa3fe4452a
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-26 21:57:50 +0900

    feat(api-client): add user api

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,8 +1,17 @@
 import { User, UserCreate, apiClient } from './core';
 
-export const createUser = async (user: UserCreate) => {
-  return await apiClient.post<User>(
+export const createUser = async (user: UserCreate) =>
+  await apiClient.post<User>(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/user/create`,
     user
   );
-};
+
+export const readUser = async (user_id: string) =>
+  await apiClient.get<User>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/user/${user_id}`
+  );
+
+export const deleteUser = async (user_id: string) =>
+  await apiClient.delete<User>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/user/${user_id}`
+  );

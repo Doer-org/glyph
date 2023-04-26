17247fb9311b0347c4ca6abc258d6a87f63d7f87
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-26 21:55:02 +0900

    feat(api-client): add glyph api

--- client/src/api/core/types/glyph.ts
+++ client/src/api/core/types/glyph.ts
@@ -1,30 +1,63 @@
 import { JTDDataType } from 'ajv/dist/core';
 
+const glyphStatus = ['Draft', 'Private', 'Public'] as const;
+
 const glyphSchema = {
   properties: {
     author_id: { type: 'string' },
     id: { type: 'string' },
     title: { type: 'string' },
     content: { type: 'string' },
+    status: { enum: glyphStatus },
     prev_glyph: { type: 'string' },
     next_glyph: { type: 'string' },
     created_at: { type: 'string' },
     updated_at: { type: 'string' },
   },
 } as const;
-
-export type Glyph = JTDDataType<typeof glyphSchema>;
 
 const glyphCreateSchema = {
   properties: {
     author_id: { type: 'string' },
     title: { type: 'string' },
     content: { type: 'string' },
+    status: { enum: glyphStatus },
     prev_glyph: { type: 'string' },
     next_glyph: { type: 'string' },
-    created_at: { type: 'string' },
-    updated_at: { type: 'string' },
   },
 } as const;
 
+const glyphReadSchema = {
+  properties: {
+    id: { type: 'string' },
+  },
+} as const;
+
+const glyphEditSchema = {
+  properties: {
+    id: { type: 'string' },
+    title: { type: 'string' },
+    content: { type: 'string' },
+    status: { enum: glyphStatus },
+    prev_glyph: { type: 'string' },
+    next_glyph: { type: 'string' },
+  },
+} as const;
+
+const glyphDeleteSchema = {
+  properties: {
+    id: { type: 'string' },
+  },
+} as const;
+
+export type Glyph = JTDDataType<typeof glyphSchema>;
+
 export type GlyphCreate = JTDDataType<typeof glyphCreateSchema>;
+
+export type GlyphRead = JTDDataType<typeof glyphReadSchema>;
+
+export type GlyphReadRelativeAll = JTDDataType<typeof glyphReadSchema>;
+
+export type GlyphEdit = JTDDataType<typeof glyphEditSchema>;
+
+export type GlyphDelete = JTDDataType<typeof glyphDeleteSchema>;
--- client/src/api/glyph.ts
+++ client/src/api/glyph.ts
@@ -1,8 +1,40 @@
-import { Glyph, GlyphCreate, apiClient } from './core';
+import {
+  Glyph,
+  GlyphCreate,
+  GlyphDelete,
+  GlyphEdit,
+  GlyphRead,
+  apiClient,
+} from './core';
 
-export const createGlyph = async (glyph: GlyphCreate) => {
-  return await apiClient.post<Glyph>(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs`,
+export const createGlyph = async (glyph: GlyphCreate) =>
+  await apiClient.post<Glyph>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/`,
     glyph
   );
-};
+
+export const readGlyph = async (glyph: GlyphRead) =>
+  await apiClient.get<Glyph>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/${glyph.id}`
+  );
+
+export const readAllGlyphs = async () =>
+  await apiClient.get<Glyph[]>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/list`
+  );
+
+export const readRelativeAllGlyphs = async (glyph: GlyphRead) =>
+  await apiClient.get<Glyph[]>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/list/${glyph.id}`
+  );
+
+export const editGlyph = async (glyph: GlyphEdit) =>
+  await apiClient.put<Glyph>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/`,
+    glyph
+  );
+
+export const deleteGlyph = async (glyph: GlyphDelete) =>
+  await apiClient.delete<Glyph>(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/glyphs/${glyph.id}`
+  );

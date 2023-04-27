96ad292576aa827bdc0e68e410bee7feef1155bf
miso-devel <kengo279@icloud.com>
2023-04-28 05:45:44 +0900

    カラム名の解決

--- client/src/app/service/glyphs/[glyph_id]/edit/page.tsx
+++ client/src/app/service/glyphs/[glyph_id]/edit/page.tsx
@@ -23,9 +23,9 @@ export default function Edit({ params }: TProps) {
 		prev_glyph: '1',
 		next_glyph: '2',
 		status: 'Draft',
-		isStudy: false,
-		created_at: new Date(),
-		updated_at: new Date(),
+		is_study: false,
+		created_at: new Date().toISOString(),
+		updated_at: new Date().toISOString(),
 	};
 	return (
 		<>

ec053c861cec54bed885ac9041eb0197fd74e6ac
miso-devel <kengo279@icloud.com>
2023-04-29 00:16:33 +0900

    button追加

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,22 +1,28 @@
-import { Title } from '@/components/atoms/Title';
-import { Txt } from '@/components/atoms/Txt';
-import { AnimatedSvgLogo } from '@/components/atoms/svgs/AnimatedSvgLogo';
-import { type NextPage } from 'next';
+"use client";
+import { Button } from "@/components/atoms/Button";
+import { Title } from "@/components/atoms/Title";
+import { Txt } from "@/components/atoms/Txt";
+import { AnimatedSvgLogo } from "@/components/atoms/svgs/AnimatedSvgLogo";
+import { type NextPage } from "next";
 
 const About: NextPage = () => {
-  return (
-    <main className="text-center pt-40">
-      <Txt elm="h1" size="text-6xl" weight="font-bold">
-        <Title stroke />
-      </Txt>
-      <Txt elm="p" className="my-4" size="text-2xl">
-        Glyph is a document sharing application
-      </Txt>
+	return (
+		<main className="text-center pt-40">
+			<Txt elm="h1" size="text-6xl" weight="font-bold">
+				<Title stroke />
+			</Txt>
+			<Txt elm="p" className="my-4" size="text-2xl">
+				Glyph is a document sharing application
+			</Txt>
 
-      <div className="m-auto">
-        <AnimatedSvgLogo />
-      </div>
-    </main>
-  );
+			<div className="m-auto">
+				<AnimatedSvgLogo />
+			</div>
+
+			<div className="my-5">
+				<Button onClick={() => console.log("button")}>ログインする</Button>
+			</div>
+		</main>
+	);
 };
 export default About;

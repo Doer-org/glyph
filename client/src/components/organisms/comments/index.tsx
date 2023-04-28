96c81099c3c58a31236868cbe0b2a84a0e2ed2f9
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-29 03:44:04 +0900

    feat(comment): api繋ぎ込み

--- client/src/components/organisms/comments/index.tsx
+++ client/src/components/organisms/comments/index.tsx
@@ -2,19 +2,46 @@
 import { FC, useEffect, useRef, useState } from 'react';
 import { CommentBox } from './commentBox';
 import { CommentInput } from './commentInput';
+import * as API from '@/api';
+
+type Comment = {
+  id: string;
+  author_id: string;
+  glyph_id: string;
+  contents: string;
+  created_at: string;
+  user?: {
+    id: string;
+    name: string;
+    img?: string;
+  };
+};
+
 type TProps = {
   glyphId: string;
+  user_id: string;
 };
-export const Comments: FC<TProps> = ({ glyphId }) => {
-  // TODO(aoki): glyphIdをもとにcommentをとってくる（静的）
-  // メモ：こっちはwebsocketを使わない方
-  // TODO(aoki): commentを取得してくる
-  const [comments, setComments] = useState<string[]>([
-    '普通のコメント1',
-    '普通のコメント2',
-  ]);
-  const scrollLastCommentRef = useRef<HTMLParagraphElement>(null);
 
+export const Comments: FC<TProps> = (props: TProps) => {
+  const [comments, setComments] = useState<Comment[]>([]);
+  useEffect(() => {
+    (async () => {
+      const comments = await API.getCommentsByGlyphId(props.glyphId);
+      if (comments.type === 'error') return;
+      const commentsAndUsers = await Promise.all(
+        comments.value.data.map(async (comment) => {
+          const user = await API.readUser(comment.author_id);
+          return {
+            ...comment,
+            user: (user.type === 'ok' && user.value.data) || undefined,
+          };
+        }),
+      );
+      setComments(commentsAndUsers);
+    })();
+  }, []);
+
+  const scrollLastCommentRef = useRef<HTMLParagraphElement>(null);
   useEffect(() => {
     scrollLastCommentRef?.current?.scrollIntoView();
   }, [comments]);
@@ -24,7 +51,6 @@ export const Comments: FC<TProps> = ({ glyphId }) => {
         {comments.map((comment, index) => {
           return (
             <div key={`${comment}-${index}`}>
-              {/* // TODO(aoki): この部分コンポーネント化してユーザー画像とかも見れるようにしてくれたらとても嬉しいかもしれない */}
               <p
                 className="border-2 p-2 rounded-md my-2 break-words"
                 ref={
@@ -33,13 +59,21 @@ export const Comments: FC<TProps> = ({ glyphId }) => {
                     : undefined
                 }
               >
-                {comment}
+                {comment.contents}
               </p>
             </div>
           );
         })}
       </CommentBox>
-      <CommentInput comment={comments} setComment={setComments} />
+      <CommentInput
+        sendComment={(comment) =>
+          API.postComment({
+            user_id: props.user_id,
+            glyph_id: props.glyphId,
+            contents: comment,
+          })
+        }
+      />
     </>
   );
 };

bebca3a16cb23bf48b89b502633ce112814eb3eb
yamasaki <yamasakipann0218@gmail.com>
2023-04-29 06:07:16 +0900

    test1

--- server/internal/infrastructure/persistance/comment.go
+++ server/internal/infrastructure/persistance/comment.go
@@ -2,6 +2,7 @@ package persistance
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/Doer-org/glyph/internal/domain/entity"
@@ -27,6 +28,7 @@ func (ur *CommentRepository) CreateComment(ctx context.Context, comment *entity.
 	VALUES (:id, :user_id, :glyph_id, :contents, :created_at)
 	`
 	dto := commentEntityToDto(comment)
+	log.Println(comment)
 	_, err := ur.conn.DB.NamedExecContext(ctx, query, &dto)
 	if err != nil {
 		return nil, err

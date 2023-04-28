4d01470049a5a081bdae59013d8da8e4fa379bde
Yamasaki Shotaro <88176012+seipan@users.noreply.github.com>
2023-04-29 06:06:29 +0900

    Update reviewdog.sh

--- server/reviewdog/reviewdog.sh
+++ server/reviewdog/reviewdog.sh
@@ -14,6 +14,6 @@ echo "$REVIEWDOG_YML" > .reviewdog_complexity.yml
 
 export REVIEWDOG_GITHUB_API_TOKEN="$INPUT_GITHUB_TOKEN"
 
-echo "::group:: reviewdog: cd server && go vet -vettool=\$(which logfind) ./..."
+echo "::group:: reviewdog: go vet -vettool=\$(which logfind) ./..."
 reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-check -level=info -filter-mode=file
 echo '::endgroup::'

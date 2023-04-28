72c7dab706408681588dda2e0e23f775462642dc
miso-devel <kengo279@icloud.com>
2023-04-28 15:34:34 +0900

    husky確かめ

--- client/src/app/service/glyphs/page.tsx
+++ client/src/app/service/glyphs/page.tsx
@@ -4,23 +4,23 @@ import { Txt } from '@/components/atoms/Txt';
 import { Glyphs } from '@/components/organisms/glyphs';
 
 const GlyphsPage = async () => {
- // 環境変数が読み込めてなくてURLのパースに失敗するよって感じでビルドこける
- const glyphs = await readAllGlyphs();
- if (glyphs.type === 'error') {
-  return new Error('Glyphを取得できませんでした');
- }
+  // 環境変数が読み込めてなくてURLのパースに失敗するよって感じでビルドこける
+  const glyphs = await readAllGlyphs();
+  if (glyphs.type === 'error') {
+    return new Error('Glyphを取得できませんでした');
+  }
 
- return (
-  <>
-   <Txt elm="h2" size="text-3xl" className="text-center pb-10">
-    Glyph一覧aaa
-   </Txt>
-   <div className="text-center mb-10">
-    <StyledLinkTo href="/service/glyphs/new">Glyph作成</StyledLinkTo>
-   </div>
+  return (
+    <>
+      <Txt elm="h2" size="text-3xl" className="text-center pb-10">
+        Glyph一覧
+      </Txt>
+      <div className="text-center mb-10">
+        <StyledLinkTo href="/service/glyphs/new">Glyph作成</StyledLinkTo>
+      </div>
 
-   <Glyphs glyphs={glyphs.value.data} />
-  </>
- );
+      <Glyphs glyphs={glyphs.value.data} />
+    </>
+  );
 };
 export default GlyphsPage;

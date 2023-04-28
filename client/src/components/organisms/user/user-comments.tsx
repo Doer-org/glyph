0ee6653c95258be9c5aad8be3edecbd2862cda72
miso-devel <kengo279@icloud.com>
2023-04-29 01:00:00 +0900

    遷移先変更

--- client/src/components/organisms/user/user-comments.tsx
+++ client/src/components/organisms/user/user-comments.tsx
@@ -39,7 +39,7 @@ export const UserComments: FC<UserCommentsProps> = () => {
       <div className="text-2xl">Comments一覧</div>
       {comment?.map((comment: Comment, index: number) => (
         <div key={comment.id} className="my-6">
-          <LinkTo href={`service/glyphs/${comment.glyph_id}`}>
+          <LinkTo href={`/service/glyphs/${comment.glyph_id}`}>
             <div>
               <div className="grid grid-cols-4 xl:grid-cols-7">
                 <div className="flex justify-start gap-4  xl:col-span-4 col-span-2">

0c3b7e9faaffd5e672d52e7a50ca9dc167fc0e41
yamasaki <yamasakipann0218@gmail.com>
2023-04-28 15:18:57 +0900

    fix : env upper

--- server/internal/infrastructure/discord/config.go
+++ server/internal/infrastructure/discord/config.go
@@ -19,14 +19,14 @@ func newDiscord(redirecturl string) *Discord {
 	logger := log.New()
 	discord := &Discord{
 		Config: &oauth2.Config{
-			ClientID:     os.Getenv("Discord_ID"),
-			ClientSecret: os.Getenv("Discord_SECRET"),
+			ClientID:     os.Getenv("DISCORD_ID"),
+			ClientSecret: os.Getenv("DISCORD_SECRET"),
 			Endpoint:     endpoint,
 			Scopes:       []string{"guilds", "email", "identify"},
 			RedirectURL:  redirecturl,
 		},
 	}
-	logger.Info("", map[string]interface{}{"ID": os.Getenv("Discord_ID"), "SECRET": os.Getenv("Discord_SECRET")})
+	logger.Info("", map[string]interface{}{"DISCORDID": os.Getenv("DISCORD_ID"), "SECRET": os.Getenv("DISCORD_SECRET")})
 	return discord
 }
 

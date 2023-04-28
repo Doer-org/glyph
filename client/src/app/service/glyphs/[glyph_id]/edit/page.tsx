2bd59bd9f6293dce18a4b73c27e138f202c9e8b0
miso-devel <kengo279@icloud.com>
2023-04-28 15:29:47 +0900

    husky確かめ

--- client/src/app/service/glyphs/[glyph_id]/edit/page.tsx
+++ client/src/app/service/glyphs/[glyph_id]/edit/page.tsx
@@ -2,37 +2,37 @@ import { Txt } from '@/components/atoms/Txt';
 import { TGlyph } from '@/types/Glyph';
 import dynamic from 'next/dynamic';
 const GlyphEditForm = dynamic(
-	() => import('@/components/organisms/glyphs/glyphEditForm'),
-	{ ssr: false },
+ () => import('@/components/organisms/glyphs/glyphEditForm'),
+ { ssr: false },
 );
 
 export const metadata = {
-	title: 'Glyph edit',
+ title: 'Glyph edit',
 };
 type TProps = {
-	params: { glyph_id: string };
-	searchParams: { id: string };
+ params: { glyph_id: string };
+ searchParams: { id: string };
 };
 export default function Edit({ params }: TProps) {
-	console.log(params.glyph_id);
-	const GlyphMock: TGlyph = {
-		id: '1',
-		author_id: 'uu',
-		title: 'F#勉強会',
-		content: '## aaa  \n- ggg',
-		prev_glyph: '1',
-		next_glyph: '2',
-		status: 'Draft',
-		is_study: false,
-		created_at: new Date().toISOString(),
-		updated_at: new Date().toISOString(),
-	};
-	return (
-		<>
-			<Txt elm="h2" size="text-3xl" className="text-center pb-10">
-				{GlyphMock.title}(編集中)
-			</Txt>
-			<GlyphEditForm glyph={GlyphMock} />
-		</>
-	);
+ console.log(params.glyph_id);
+ const GlyphMock: TGlyph = {
+  id: '1',
+  author_id: 'uu',
+  title: 'F#勉強会',
+  content: '## aaa  \n- ggg',
+  prev_glyph: '1',
+  next_glyph: '2',
+  status: 'Draft',
+  is_study: false,
+  created_at: new Date().toISOString(),
+  updated_at: new Date().toISOString(),
+ };
+ return (
+  <>
+   <Txt elm="h2" size="text-3xl" className="text-center pb-10">
+    {GlyphMock.title}(編集中)
+   </Txt>
+   <GlyphEditForm glyph={GlyphMock} />
+  </>
+ );
 }

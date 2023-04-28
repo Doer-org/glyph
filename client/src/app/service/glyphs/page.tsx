9688af62a8a60608de1c89e19343300e8b7c01e5
miso-devel <kengo279@icloud.com>
2023-04-28 15:26:20 +0900

    husky確かめ

--- client/src/app/service/glyphs/page.tsx
+++ client/src/app/service/glyphs/page.tsx
@@ -1,19 +1,19 @@
-import { readAllGlyphs } from '@/api/glyph';
-import { StyledLinkTo } from '@/components/atoms/StyledLinkTo';
-import { Txt } from '@/components/atoms/Txt';
-import { Glyphs } from '@/components/organisms/glyphs';
+import { readAllGlyphs } from "@/api/glyph";
+import { StyledLinkTo } from "@/components/atoms/StyledLinkTo";
+import { Txt } from "@/components/atoms/Txt";
+import { Glyphs } from "@/components/organisms/glyphs";
 
 const GlyphsPage = async () => {
 	// 環境変数が読み込めてなくてURLのパースに失敗するよって感じでビルドこける
 	const glyphs = await readAllGlyphs();
-	if (glyphs.type === 'error') {
-		return new Error('Glyphを取得できませんでした');
+	if (glyphs.type === "error") {
+		return new Error("Glyphを取得できませんでした");
 	}
 
 	return (
 		<>
 			<Txt elm="h2" size="text-3xl" className="text-center pb-10">
-				Glyph一覧
+				Glyph一覧aa
 			</Txt>
 			<div className="text-center mb-10">
 				<StyledLinkTo href="/service/glyphs/new">Glyph作成</StyledLinkTo>

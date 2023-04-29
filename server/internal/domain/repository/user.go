8a76b696290b7f0d531025b6100f5f0650ce6a42
kuma033 <nhayato28@yahoo.co.jp>
2023-04-29 12:13:21 +0900

    comments/all

--- server/internal/domain/repository/user.go
+++ server/internal/domain/repository/user.go
@@ -10,5 +10,4 @@ type IUserRepository interface {
 	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 	DeleteUser(ctx context.Context, id string) error
 	GetUser(ctx context.Context, id string) (*entity.User, error)
-	
 }

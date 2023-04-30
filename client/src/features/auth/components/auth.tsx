a0ad56754234a070f70332af859421129ab22716
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-30 16:58:15 +0900

    refactor(client): remove comment

--- client/src/features/auth/components/auth.tsx
+++ client/src/features/auth/components/auth.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
-import { getToken } from '../api/get-token';
-import { getLoggedInUser } from '@/api';
+import { getToken } from '../utils/get-token';
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +8,5 @@ type Props = {
 export const Auth = async (props: Props) => {
   const token = getToken();
   if (!token) redirect(`${process.env.NEXT_PUBLIC_FRONT_URL}`);
-  // const u = await getLoggedInUser(token);
-  // console.log(u);
   return <>{props.children}</>;
 };

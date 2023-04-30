d70df5dd813d7912e61bed03fc756a6b9ab12808
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-30 16:39:52 +0900

    refactor(auth): middlewareでルーティング制御をしない

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,18 +6,5 @@ export const config = {
 };
 
 export const middleware = async (request: NextRequest) => {
-  const cookie = request.headers.get('cookie') ?? '';
-  const resp = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/validate`,
-    {
-      method: 'GET',
-      headers: {
-        jwt: cookie.split('=')[1],
-      },
-    },
-  );
-  if (resp.status === 400) {
-    return NextResponse.redirect(`${request.nextUrl.origin}/`);
-  }
   return NextResponse.next();
 };

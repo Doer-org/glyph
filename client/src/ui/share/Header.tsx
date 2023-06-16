7f7d40eb4efd45ea8e0981b79eff554338175c73
miso-devel <kengo279@icloud.com>
2023-06-16 18:33:11 +0900

    titleをaに

--- client/src/ui/share/Header.tsx
+++ client/src/ui/share/Header.tsx
@@ -4,7 +4,9 @@ export const Header = () => {
   return (
     <header className="flex items-center gap-3 p-5 h-[10vh]">
       <SvgLogo />
-      <h1 className="font-bold text-2xl">Glyph</h1>
+      <a className="font-bold text-2xl hover:opacity-50" href="/">
+        Glyph
+      </a>
     </header>
   )
 }

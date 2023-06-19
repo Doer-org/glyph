0ff40070f0a5f140cbeac6e359d02f4e1e42d344
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-19 15:48:25 +0900

    cd: check

--- client/src/ui/Button/components/Button.tsx
+++ client/src/ui/Button/components/Button.tsx
@@ -11,7 +11,7 @@ export const Button = ({ children, className, onClick, disable }: TButton) => {
   return (
     <button
       onClick={onClick}
-      className={`border-2 rounded-md hover:bg-yellow-300 py-1 px-5 hover:scale-110 transition ${className}`}
+      className={`border-2  rounded-md hover:bg-yellow-300 py-1 px-5 hover:scale-110 transition ${className}`}
       disabled={disable}
     >
       {children}

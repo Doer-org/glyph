f313ac428282e7252e958599da4399887e8d04a0
Meow520 <cgjg0289@mail4.doshisha.ac.jp>
2023-06-22 02:16:26 +0900

    fix(client):fix styles

--- client/src/app/(service)/users/[user_id]/_component/user/index.tsx
+++ client/src/app/(service)/users/[user_id]/_component/user/index.tsx
@@ -8,14 +8,8 @@ type TProps = { user: TUser }
 
 export const User: FC<TProps> = ({ user }) => {
   return (
-    <div className="flex items-center text-center gap-5">
-      {/* <Image
-        src={user.Img}
-        width={120}
-        height={120}
-        alt={'icon'}
-        className="rounded-full"
-      /> */}
+    <div className="flex items-center text-center gap-5 mb-4">
+      <Image src={user.Img} width={120} height={120} alt={'icon'} className="rounded-full" />
       <h1 className="text-3xl font-bold">{user.Name}</h1>
     </div>
   )

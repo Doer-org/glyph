76b5198a5d53977f899e54c0531d150224ed52d5
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-22 21:27:58 +0900

    revert(client): url

--- client/src/app/(top)/_components/discordLink/index.tsx
+++ client/src/app/(top)/_components/discordLink/index.tsx
@@ -1,7 +1,7 @@
 import { getEnv } from '@/utils'
 const { serverURL, clientURL } = getEnv()
 export const DiscordLink = () => {
-  const url = `${serverURL}/auth/login?redirect_url=${clientURL}//glyphs`
+  const url = `${serverURL}/auth/login?redirect_url=${clientURL}/glyphs`
   return (
     <a href={url} className="py-3 px-4 bg-blue-700 rounded-md text-white font-bold hover:opacity-90">
       Discordでログインする !

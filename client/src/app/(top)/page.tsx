3dfea499eba9ac773191094e0f269f2507846274
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-22 14:52:41 +0900

    refactor(client): 不要なapi call

--- client/src/app/(top)/page.tsx
+++ client/src/app/(top)/page.tsx
@@ -1,17 +1,7 @@
-import { getToken } from '@/api/utils/token'
-import { getEnv } from '@/utils'
-
 import { DiscordLink } from './_components/discordLink'
 import { IntroduceContents } from './_components/introduceContents'
 
-const { serverURL } = getEnv()
 const Top = async () => {
-  const resp = await fetch(`${serverURL}/auth/validate`, {
-    method: 'GET',
-    headers: { jwt: getToken() },
-  })
-  const isValid = resp.status !== 400
-
   return (
     <>
       <IntroduceContents />

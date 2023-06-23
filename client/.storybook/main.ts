13f7a3ddfeeeb1dad64118d80d17b9e995df4c7e
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-24 00:59:17 +0900

    fix(storybook): alias

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -19,6 +20,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+      }
+    }
+    return config
   },
 }
 export default config

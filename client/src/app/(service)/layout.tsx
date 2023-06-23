0aab3db681656f698687b39cec555ee9282afdcf
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-23 21:36:47 +0900

    feat(client): header & avatar

--- client/src/app/(service)/layout.tsx
+++ client/src/app/(service)/layout.tsx
@@ -1,10 +1,21 @@
-import { Auth } from '@/features'
+import { getLoggedInUser } from '@/api'
+import { Auth, getToken } from '@/features'
+import { Header } from '@/ui/share/Header'
 
-export default function ServiceLayout({ children }: { children: React.ReactNode }) {
+export default async function ServiceLayout({ children }: { children: React.ReactNode }) {
+  const user = await (async () => {
+    const user = await getLoggedInUser(getToken() || '')
+    if (user.type === 'error') return undefined
+    return user.value.user
+  })()
+
   return (
     <>
       {/* @ts-expect-error Server Component */}
-      <Auth>{children}</Auth>
+      <Auth>
+        <Header user={user} />
+        <main>{children}</main>
+      </Auth>
     </>
   )
 }
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Murecho } from 'next/font/google'
 
+import { getLoggedInUser } from '@/api'
+import { getToken } from '@/features'
 import { Header } from '@/ui/share/Header'
 import './globals.css'
 const murecho = Murecho({ weight: '100', subsets: ['latin'] })
@@ -9,13 +11,10 @@ export const metadata = {
   description: 'Glyph is a document sharing application.',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body style={{ fontFamily: `${murecho}` }}>
-        <Header />
-        <main>{children}</main>
-      </body>
+      <body style={{ fontFamily: `${murecho}` }}>{children}</body>
     </html>
   )
 }

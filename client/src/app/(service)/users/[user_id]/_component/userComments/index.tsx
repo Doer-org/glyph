83fefad465658bff9a87b577f3e607888c9ca9ad
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-23 23:41:27 +0900

    fix(client): infinity render

--- client/src/app/(service)/users/[user_id]/_component/userComments/index.tsx
+++ client/src/app/(service)/users/[user_id]/_component/userComments/index.tsx
@@ -1,36 +1,37 @@
 'use client'
-import { FC, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { readGlyph } from '@/api'
 import { TComment } from '@/types/Comment'
 import { PopLinkTo } from '@/ui/LinkTo/components/popLinkTo'
 
 type UserCommentsProps = {
-  comments: TComment[] | null
+  comments: TComment[]
 }
 
-export const UserComments: FC<UserCommentsProps> = ({ comments }) => {
-  const [glyphTitles, setGlyphTitles] = useState<String[] | null>(null)
-  comments?.map(async (comment: TComment) => {
-    const glyphTitleResp = await readGlyph(comment.glyph_id)
-    if (glyphTitleResp.type === 'error') return
-    const glyphTitle = glyphTitleResp.value.data.title
-    const new_glyphTitle = glyphTitles ? [...glyphTitles, glyphTitle] : [glyphTitle]
-    setGlyphTitles(new_glyphTitle)
-  })
+export const UserComments = ({ comments }: UserCommentsProps) => {
+  const [id2GlyphTitle, setId2GlyphTitle] = useState<Record<string, string>>({})
+  useEffect(() => {
+    const r = (async () => {
+      const glyphIds = new Set(comments.map((comment) => comment.glyph_id))
+      const glyphs = await Promise.all(Array.from(glyphIds).map((glyphId) => readGlyph(glyphId)))
+      return glyphs
+        .flatMap((g) => (g.type === 'error' ? [] : g.value.data))
+        .map<[string, string]>((g) => [g.id, g.title])
+        .reduce<Record<string, string>>((acc, [id, title]) => ({ ...acc, [id]: title }), {})
+    })()
+    r.then(setId2GlyphTitle)
+  }, [comments])
+
   return (
     <div className="flex flex-col gap-5">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-        {comments ? (
-          comments.map((comment: TComment, index: number) => (
-            <PopLinkTo href={`/glyphs/${comment.glyph_id}`} className="block w-full" key={comment.id}>
-              <h2 className="text-2xl font-bold">{comment.contents}</h2>
-              {glyphTitles && <p className="mt-auto mb-0"> - {glyphTitles[index]}</p>}
-            </PopLinkTo>
-          ))
-        ) : (
-          <p>まだコメントしていません</p>
-        )}
+        {comments.map((comment: TComment) => (
+          <PopLinkTo href={`/glyphs/${comment.glyph_id}`} className="block w-full" key={comment.id}>
+            <h2 className="text-2xl font-bold">{comment.contents}</h2>
+            <p className="mt-auto mb-0"> - {comment.glyph_id in id2GlyphTitle && id2GlyphTitle[comment.glyph_id]}</p>
+          </PopLinkTo>
+        ))}
       </div>
     </div>
   )

f97df8d672e9833f85905157af5e3d506cdfc95e
yamasaki <yamasakipann0218@gmail.com>
2023-08-06 23:19:23 +0900

    feat : nosend debug

--- server/log/log.go
+++ server/log/log.go
@@ -8,6 +8,7 @@ import (
 
 func New() *loghook.Logger {
 	logger := loghook.NewLogger(os.Getenv("LOGIMG"), os.Getenv("LOGNAME"), "discord", os.Getenv("WEBHOOKURL"))
+	logger.NoSendDebug()
 	logger.SetErrorWebhook(os.Getenv("ERRORWEBHOOKURL"))
 	return logger
 }
